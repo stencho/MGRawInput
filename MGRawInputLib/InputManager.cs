b8efed16bf39ead55e9a2c3a6ff920b0a34ccdc6
Astronat <c3142113@gmail.com>
2023-05-24 19:04:57 +1000

    Basic monogame input polling thread/manager for fallback later on and basic functionality now

---
+++ MGRawInputLib/InputManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MGRawInputLib.InputTypes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using static MGRawInputLib.InputStructs;
+
+namespace MGRawInputLib {
+    //This entire class exists to "decouple" the input handling from the polling thread
+    //Basically, if the polling thread handles the previous/current state, just_pressed
+    //and just_released won't work correctly, as, to put it in some kinda way, the rising
+    //edge of the pressed state is happening between frames
+    //If the main thread is used for polling, then any threads used for updating will have
+    //the same issue if they're running slower than it, or the opposite problem, too much
+    //just_pressed, if they're running faster
+
+    //The program will crash if say Keyboard.GetState() gets called in two different threads
+    //around the same time, so we have one high rate thread to poll the inputs, then other
+    //threads copy from it and handle their own previous state
+
+    //same thing kind of applies for mouse_delta
+
+    public class InputManager {
+        public Vector2 mouse_delta_integer { get; private set; }
+        public Vector2 mouse_position { get; private set; }
+
+        KeyboardState keyboard_state; KeyboardState keyboard_state_previous;
+
+        MouseState mouse_state; MouseState mouse_state_previous;
+
+        GamePadState gamepad_one_state; GamePadState gamepad_one_state_previous;
+        GamePadState gamepad_two_state; GamePadState gamepad_two_state_previous;
+        GamePadState gamepad_three_state; GamePadState gamepad_three_state_previous;
+        GamePadState gamepad_four_state; GamePadState gamepad_four_state_previous;
+
+        public void update() {
+            keyboard_state_previous = keyboard_state;
+            keyboard_state = InputPolling.keyboard_state;
+            
+            mouse_state_previous = mouse_state;
+            mouse_state = InputPolling.mouse_state;
+
+            gamepad_one_state_previous = gamepad_one_state;
+            gamepad_one_state = InputPolling.gamepad_one_state;
+
+            gamepad_two_state_previous = gamepad_two_state;
+            gamepad_two_state = InputPolling.gamepad_two_state;
+
+            gamepad_three_state_previous = gamepad_three_state;
+            gamepad_three_state = InputPolling.gamepad_three_state;
+
+            gamepad_four_state_previous = gamepad_four_state;
+            gamepad_four_state = InputPolling.gamepad_four_state;
+
+            mouse_position = mouse_state.Position.ToVector2();
+            mouse_delta_integer = (mouse_state.Position - mouse_state_previous.Position).ToVector2();
+        }
+
+        public bool is_pressed(Keys key) {
+            return keyboard_state.IsKeyDown(key);            
+        }
+        public bool was_pressed(Keys key) {
+            return keyboard_state_previous.IsKeyDown(key);
+        }
+        public bool is_pressed(MouseButtons mouse_button) {
+            switch (mouse_button) {
+                case MouseButtons.Left:
+                    return mouse_state.LeftButton == ButtonState.Pressed;
+                case MouseButtons.Right:
+                    return mouse_state.RightButton == ButtonState.Pressed;
+                case MouseButtons.Middle:
+                    return mouse_state.MiddleButton == ButtonState.Pressed;
+                case MouseButtons.X1:
+                    return mouse_state.XButton1 == ButtonState.Pressed;
+                case MouseButtons.X2:
+                    return mouse_state.XButton2 == ButtonState.Pressed;
+                default: return false;
+            }
+        }
+        public bool was_pressed(MouseButtons mouse_button) {
+            switch (mouse_button) {
+                case MouseButtons.Left:
+                    return mouse_state_previous.LeftButton == ButtonState.Pressed;
+                case MouseButtons.Right:
+                    return mouse_state_previous.RightButton == ButtonState.Pressed;
+                case MouseButtons.Middle:
+                    return mouse_state_previous.MiddleButton == ButtonState.Pressed;
+                case MouseButtons.X1:
+                    return mouse_state_previous.XButton1 == ButtonState.Pressed;
+                case MouseButtons.X2:
+                    return mouse_state_previous.XButton2 == ButtonState.Pressed;
+                default: return false;
+            }
+        }
+
+        public bool just_pressed(Keys key) => is_pressed(key) && !was_pressed(key);        
+        public bool just_released(Keys key) => !is_pressed(key) && was_pressed(key);
+
+        public bool just_pressed(MouseButtons mouse_button) => is_pressed(mouse_button) && !was_pressed(mouse_button);
+        public bool just_released(MouseButtons mouse_button) => !is_pressed(mouse_button) && was_pressed(mouse_button);
+    }
+}
---
+++ MGRawInputLib/InputPolling.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGRawInputLib {
+    public static class InputPolling {
+        static Thread control_update_thread = new Thread(new ThreadStart(update));
+
+        public static void initialize() { control_update_thread.Start(); }
+        public static void kill() { run_thread = false; }
+
+
+        public static KeyboardState keyboard_state {get; private set; }
+        public static MouseState mouse_state { get; private set; }
+
+        public static GamePadState gamepad_one_state { get; private set; }
+        public static GamePadState gamepad_two_state { get; private set; }
+        public static GamePadState gamepad_three_state { get; private set; }
+        public static GamePadState gamepad_four_state { get; private set; }
+
+        public static int frame_rate => _frame_rate;
+        static int _frame_rate;
+
+        static double _fps_timer = 0;
+        static long _frame_count = 0;
+
+        public static int fps_update_frequency_ms { get; set; } = 500;
+        public static int poll_hz { get; private set; } = 1000;
+
+        static double thread_ms => (1000.0 / poll_hz);
+
+        static DateTime start_dt = DateTime.Now;
+        static DateTime current_dt = DateTime.Now;
+        static TimeSpan ts;
+
+        static volatile bool run_thread = true;
+        public static void update() {
+            while (run_thread) {
+                start_dt = DateTime.Now;
+
+                keyboard_state = Keyboard.GetState();
+                mouse_state = Mouse.GetState();
+
+                gamepad_one_state = GamePad.GetState(PlayerIndex.One);
+                gamepad_two_state = GamePad.GetState(PlayerIndex.Two);
+                gamepad_three_state = GamePad.GetState(PlayerIndex.Three);
+                gamepad_four_state = GamePad.GetState(PlayerIndex.Four);
+
+                while (run_thread) {
+                    current_dt = DateTime.Now;
+                    ts = (current_dt - start_dt);
+                    if (ts.TotalMilliseconds >= thread_ms) break;
+                }
+
+                //FPS stuff here
+                _fps_timer += ts.TotalMilliseconds;
+                _frame_count++;
+
+                if (_fps_timer >= fps_update_frequency_ms) {
+                    _frame_rate = (int)(_frame_count * (1000.0 / fps_update_frequency_ms));
+                    _frame_count = 0;
+                    _fps_timer -= fps_update_frequency_ms;
+                }
+            }
+        }
+
+        static void reset_mouse() {
+
+        }
+
+    }
+}
